7f72a29f893c4be18b6c6e22c88c7f43c8734554
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2022-03-13 16:45:40 +1100

    Update autorun.js

--- autorun.js
+++ autorun.js
@@ -1,4 +1,4 @@
 w=document.createElement("button");
 w.class = "btn-default";
 w.value = "It works!"
-document.getElementsByClassName("btns").appendChild("w");
+document.getElementsByClassName("btns").appendChild(w);
